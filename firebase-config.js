16667b8683e56f59d6b30d0c77cdd8fc898abf24
Gerkaxa <fliarneodoger@gmail.com>
2025-07-03 10:23:16 -0300

    Update firebase-config.js
    seguridad borre la apikey

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDGKHIZSPV8cUbiAN4uskWHd_ePBn-hFdE",
-  authDomain: "bellitalia-viajes.firebaseapp.com",
-  projectId: "bellitalia-viajes",
-  storageBucket: "bellitalia-viajes.firebasestorage.app",
-  messagingSenderId: "718574565165",
-  appId: "1:718574565165:web:19a11d2971f19bbc9c05a4",
-  measurementId: "G-2Q5BKCF0GJ"
-};
+
